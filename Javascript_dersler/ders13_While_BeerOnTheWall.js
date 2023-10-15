178c664184680ec0cf418aa28a60eb6bf88e615d
MrGitharist <malasmustafa99@gmail.com>
2023-10-16 01:45:39 +0300

    Js ders 13

--- Javascript_dersler/ders13_While_BeerOnTheWall.js
+++ Javascript_dersler/ders13_While_BeerOnTheWall.js
@@ -1,7 +1,9 @@
-var count;
-
 function bottles (beerQuantity){
-    while (count <=beerQuantity){
-        console.log("");
+    var bottleNumber = beerQuantity;
+    while (bottleNumber > 0){
+        console.log(bottleNumber + " bottles of beer on the wall, " + bottleNumber + " bottles of beer.\nTake one down and pass it around, " + bottleNumber + " bottles of beer on the wall.");
+        bottleNumber--;
     }
+    console.log("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, " + bottleNumber + " bottles of beer on the wall.\n");
 }
+var count = bottles(10);
