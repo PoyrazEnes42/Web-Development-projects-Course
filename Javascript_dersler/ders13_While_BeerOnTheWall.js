4b4ed2521ccad6f55194bf4b19d1f9b27eed8774
MrGitharist <malasmustafa99@gmail.com>
2023-10-16 01:54:03 +0300

    ders 13

--- Javascript_dersler/ders13_While_BeerOnTheWall.js
+++ Javascript_dersler/ders13_While_BeerOnTheWall.js
@@ -1,9 +1,15 @@
 function bottles (beerQuantity){
     var bottleNumber = beerQuantity;
     while (bottleNumber > 0){
-        console.log(bottleNumber + " bottles of beer on the wall, " + bottleNumber + " bottles of beer.\nTake one down and pass it around, " + bottleNumber + " bottles of beer on the wall.");
+        if (bottleNumber === 1){
+            console.log(bottleNumber + " bottles of beer on the wall, " + bottleNumber + " bottles of beer.\nTake one down and pass it around, no more bottles of beer on the wall.");
+            bottleNumber--;
+        }else{
+        console.log(bottleNumber + " bottles of beer on the wall, " + bottleNumber + " bottles of beer.\nTake one down and pass it around, " + (bottleNumber-1) + " bottles of beer on the wall.");
         bottleNumber--;
+        }
     }
-    console.log("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, " + bottleNumber + " bottles of beer on the wall.\n");
+    console.log("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, " + beerQuantity + " bottles of beer on the wall.\n");
 }
 var count = bottles(10);
+console.log(count);
