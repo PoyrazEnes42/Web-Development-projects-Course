d0a38f572cf2a6dcf94af80fe5938cdfc6fa1a42
MrGitharist <malasmustafa99@gmail.com>
2023-10-20 01:13:38 +0300

    Dice Game

--- Javascript_lessons/AfterHtml/Dicee Challenge - Starting Files/index.js
+++ Javascript_lessons/AfterHtml/Dicee Challenge - Starting Files/index.js
@@ -1,2 +1,19 @@
-var randomNumber = Math.floor(Math.random()*6) + 1;
- 
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage1 = "./images/dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png";
+var img1 = document.querySelectorAll("img")[0];
+var img2 = document.querySelectorAll("img")[1];
+img1.setAttribute("src", randomDiceImage1);
+img2.setAttribute("src", randomDiceImage2);
+
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "<i class='fa-solid fa-flag'></i> Player 1 Wins";
+}
+else if (randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 Wins <i class='fa-solid fa-flag'></i>"; 
+}
+else {
+    document.querySelector("h1").innerHTML = "draw";
+}
