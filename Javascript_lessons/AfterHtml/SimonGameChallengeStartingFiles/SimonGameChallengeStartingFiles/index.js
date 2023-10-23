b404f963b2fe446c39d8ef92e1259409a9b104fa
MrGitharist <malasmustafa99@gmail.com>
2023-10-23 15:19:31 +0300

    simon Game

--- Javascript_lessons/AfterHtml/SimonGameChallengeStartingFiles/SimonGameChallengeStartingFiles/index.js
+++ Javascript_lessons/AfterHtml/SimonGameChallengeStartingFiles/SimonGameChallengeStartingFiles/index.js
@@ -1,109 +1,62 @@
-//3. At the top of the game.js file, create a new array called buttonColours and set it to hold the sequence "red", "blue", "green", "yellow" .
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-//5. At the top of the game.js file, create a new empty array called gamePattern.
 var gamePattern = [];
-
-//3. At the top of the game.js file, create a new empty array with the name userClickedPattern.
 var userClickedPattern = [];
 
-//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
 var started = false;
-
-//2. Create a new variable called level and start at level 0.
 var level = 0;
 
 $(document).keypress(function() {
   if (!started) {
-
-    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
 
-$(".btn").click(function () {
-  //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
+$(".btn").click(function() {
+
   var userChosenColour = $(this).attr("id");
-
-  //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
   userClickedPattern.push(userChosenColour);
-
-
-
-  console.log(userClickedPattern);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  //2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
   checkAnswer(userClickedPattern.length-1);
 });
 
-//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
 function checkAnswer(currentLevel) {
 
-  //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+      if (userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+      }
+    } else {
+      playSound("wrong");
+      $("body").addClass("game-over");
+      $("#level-title").text("Game Over, Press Any Key to Restart");
 
-    console.log("success");
-
-    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
-    if (userClickedPattern.length === gamePattern.length){
-
-      //5. Call nextSequence() after a 1000 millisecond delay.
       setTimeout(function () {
-        nextSequence();
-      }, 1000);
+        $("body").removeClass("game-over");
+      }, 200);
 
+      startOver();
     }
-
-  } else {
-
-    console.log("wrong");
-
-  }
-
 }
 
-//1. Inside game.js create a new function called nextSequence()
+
 function nextSequence() {
-  //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
   userClickedPattern = [];
-
-  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
   level++;
-
-  //5. Inside nextSequence(), update the h1 with this change in the value of level.
   $("#level-title").text("Level " + level);
-
-  //2. Inside the new function generate a new random number between 0 and 3, and store it in a variable called randomNumber
   var randomNumber = Math.floor(Math.random() * 4);
-
-  //4. Create a new variable called randomChosenColour and use the randomNumber from step 2 to select a random colour from the buttonColours array.
   var randomChosenColour = buttonColours[randomNumber];
-
-  //6. Add the new randomChosenColour generated in step 4 to the end of the gamePattern.
   gamePattern.push(randomChosenColour);
 
-  //1. Use jQuery to select the button with the same id as the randomChosenColour
-  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
-  $("#" + randomChosenColour)
-    .fadeIn(100)
-    .fadeOut(100)
-    .fadeIn(100);
-
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColour);
-
-  console.log(randomChosenColour);
-}
-
-//2. Create a new function called playSound() that takes a single input parameter called name.
-function playSound(name) {
-  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
 }
 
 function animatePress(currentColor) {
@@ -111,4 +64,15 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
