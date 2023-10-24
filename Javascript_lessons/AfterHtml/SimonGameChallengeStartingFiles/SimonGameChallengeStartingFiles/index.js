95015d5d007154f259e8f9a7f88170ae4b9b00ee
MrGitharist <malasmustafa99@gmail.com>
2023-10-24 06:08:04 +0300

    sdfsdf

--- Javascript_lessons/AfterHtml/SimonGameChallengeStartingFiles/SimonGameChallengeStartingFiles/index.js
+++ Javascript_lessons/AfterHtml/SimonGameChallengeStartingFiles/SimonGameChallengeStartingFiles/index.js
@@ -82,95 +82,32 @@ function startOver() {
 
 */
 
-var buttonColours = ["red", "blue", "yellow", "green"];
+var butonRenkler = ["red", "green", "blue", "yellow"];
 var level = 0;
-var gamePattern = [];
-var started = false;
-var userClickedPattern = [];
+var kullaniciSecilen = [];
+var oyunSecmesiGereken = [];
+var Basladi = false;
 
-$(document).keydown(function () {
-  if (!started) {
-    $("#level-title").text("level" + level);
-    nextSequence();
-    started = true;
-  }
-});
 
-$(".btn").click(function () {
-  var userChosenColour = $(this).attr("id");
-  userClickedPattern.push(userChosenColour);
-  console.log(userClickedPattern);
-  console.log(gamePattern);
-  playSound(userChosenColour);
-  animatePress(userChosenColour);
-  checkAnswer(userClickedPattern.length - 1);
-});
 
-function checkAnswer(currentLevel) {
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    if (userClickedPattern.length === gamePattern.length) {
-      setTimeout(function () {
-        nextSequence();
-      }, 1000);
-    }
-  } else {
-    playSound("wrong");
-    $("body").addClass("game-over");
-    $("#level-title").text("Game Over, Press Any Key to Restart");
-    setTimeout(function () {
-      $("body").removeClass("game-over");
-    }, 200);
-    startOver();
-  }
-}
-
-function checkAnswer(currentLevel) {
-  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    if (userClickedPattern.length === gamePattern.length) {
-      setTimeout(function () {
-        nextSequence();
-      }, 1000);
-    } 
-  }
-  else {
-    playSound("wrong");
-    $("body").addClass("game-over");
-    $("#level-title").text("Game Over, Press Any Key to Restart");
-    setTimeout(function () {
-      $("body").removeClass("game-over");
-    }, 200);
-    startOver();
-  }
-}
-
-function nextSequence() {
-  userClickedPattern = [];
+function yeniSeans() {
+  kullaniciSecilen = [];
   level++;
-  $("#level-title").text("level " + level);
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour)
-    .fadeIn(100)
-    .fadeOut(100)
-    .fadeIn(100);
-  playSound(randomChosenColour);
+  var randomSayi = Math.floor(Math.random() * 4);
+  var randomSecilenRenk = butonRenkler[randomSayi];
+  oyunSecmesiGereken.push(randomSecilenRenk);
+  $("#"+randomSecilenRenk).fadeIn(100).fadeOut(100).fadeIn(100);
+  sesOynat(randomSecilenRenk);
+  animation(randomSecilenRenk);
 }
 
-function playSound(sound) {
-  var audio = new Audio("sounds/" + sound + ".mp3");
-  audio.play();
+function sesOynat(renk) {
+  var ses = new Audio("./sounds/" + renk + ".mp3");
+  ses.play();
 }
-
-function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false;
-}
-
-function animatePress(nowColor) {
-  $("#" + nowColor).addClass("pressed");
+function animation(renk) {
+  $(".btn").addClass("pressed");
   setTimeout(function () {
-    $("#" + nowColor).removeClass("pressed");
+    $(".btn").removeClass("pressed");
   }, 100);
 }
